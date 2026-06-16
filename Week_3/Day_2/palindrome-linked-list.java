13d7186f7e0c1363bf65d77d187fc8c471e5e554
tushaxy <tusharjeswani567@gmail.com>
2026-06-16 22:02:27 +0530

    Create palindrome-linked-list.java

---
+++ Week_3/Day_2/palindrome-linked-list.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        ListNode prev = null;
+        ListNode curr = slow;
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        
+        ListNode firstHalf = head;
+        ListNode secondHalf = prev;
+        while (secondHalf != null) {
+            if (firstHalf.val != secondHalf.val) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+        
+        return true;
+    }
+}
