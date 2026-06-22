fd1155e9721514bc0525991bc8f5a89513d9fc92
tushaxy <tusharjeswani567@gmail.com>
2026-06-22 23:06:02 +0530

    Create invert-binary-tree.java

---
+++ Week_4/Day_1/invert-binary-tree.java
@@ -0,0 +1,14 @@
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        
+        TreeNode tempNode = root.left;
+        
+        root.left = invertTree(root.right);
+        root.right = invertTree(tempNode);
+        
+        return root;
+    }
+}
